78e7718d7969234099e52641c2d7fb52fe740bb0
Meth831 <DinuX01Ekanayaka@gmail.com>
2025-01-21 22:01:26 +0530

    Create Adminpage.jsx file on components and import it App.jsx✅

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,16 @@
 import './App.css'
-import ProductCard from './components/productCard'
+import AdminPage from './components/Adminpage';
+
+
 
 function App() {
-  
 
   return (
     <div>
-
-      <ProductCard name="Audio Setup" price="3250/-" 
-
-      description="loremasd adfj asld as fasdf sadf" 
-
-      photoUrl="https://img.drz.lazcdn.com/g/kf/S6cfb0ee8d9f74ebb9660fd04b3622e364.jpg_720x720q80.jpg_.webp"  
-      
-      />
-      <ProductCard name="Retro Mini Game Machine Console"
-
-      price="2374/-"
-
-      description = "500 IN 1 Retro Mini Game Machine Console Gameboy Rechargeable Portable Video Handheld Ma-rio PSP Built-in 400 Games"
-
-      photoUrl="https://img.drz.lazcdn.com/g/kf/S7fa0aa8213b44f91a73ca3bb21c9e3266.jpg_720x720q80.jpg_.webp"/>
-
+      <AdminPage/>
     </div>
   )
+
 }
 
 export default App
