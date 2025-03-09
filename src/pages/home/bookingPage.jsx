6735b363b5f93b63c10615abcec673a24d704050
Methmi_Ekanayaka <DinuX01Ekanayaka@gmail.com>
2025-03-09 14:16:41 +0530

    completed bookingItem.jsx✅

--- src/pages/home/bookingPage.jsx
+++ src/pages/home/bookingPage.jsx
@@ -1,7 +1,24 @@
+import { useState } from "react"
+import { loadCart } from "../../utils/cart";
+import BookingItem from "../../components/bookingItem";
+
 export default function BookingPage(){
+    const [cart,setCart]=useState(loadCart());
+
+    function reloadCart(){
+        setCart(loadCart());
+    }
     return(
-        <div>
-            <h1>Booking page</h1>
+        <div className="w-full h-full flex flex-col items-center">
+            <h1> create Booking page</h1>
+            <div className="w-full flex flex-col items-center">
+                {
+                    cart.orederdItems.map((item)=>{
+                        return <BookingItem itemkKey={item.key} qty={item.qty}refresh={reloadCart}/>
+                    })
+                }
+
+            </div>
         </div>
     )
 }
