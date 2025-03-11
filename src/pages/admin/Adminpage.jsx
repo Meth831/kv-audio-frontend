387e3d56f7f5d54347074468ab284b3c5ae3849b
Methmi_Ekanayaka <DinuX01Ekanayaka@gmail.com>
2025-03-11 10:19:17 +0530

    Day 20 💛

--- src/pages/admin/Adminpage.jsx
+++ src/pages/admin/Adminpage.jsx
@@ -6,10 +6,44 @@ import { Link, Routes, Route } from 'react-router-dom';
 import AdminItemsPage from "./adminItemsPage";
 import AddItemPage from "./addItemPage";
 import UpdateItemPage from "./updateItmPage";
+import AdminUsersPage from "./adminUsersPage";
+
+import AdminOrdersPage from "./adminBookingPage";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 
 
 export default function AdminPage() {
+  const[userValidated,setUserValidated]=useState(false);
+
+  useEffect(()=>{
+    const token=localStorage.getItem("token");
+    if(!token){
+      window.location.href="/login";
+    }
+
+    axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/users/`,{
+      headers:{
+        Authorization:`Bearer ${token}`
+      }
+    }).then((res)=>{
+      console.log(res.data);
+      const user=res.data;
+      if(user.role!=="admin"){
+        setUserValidated(true);
+        
+      }else{
+        window.location.href="/";
+      }
+      
+    }).catch((err)=>{
+      console.error(err);
+      setUserValidated(false);
+    })
+
+
+  },[])
   return (
     <div className='w-full h-screen flex'>
       <div className='w-[200px] h-full bg-gray-200'>
@@ -17,28 +51,29 @@ export default function AdminPage() {
           <BsGraphDown />
           Dashboard
         </button>
-        <Link to="/admin/bookings" className='w-full h-[40px] text-[25px] font-bold flex justify-center items-center'>
+        <Link to="/admin/orders" className='w-full h-[40px] text-[25px] font-bold flex justify-center items-center'>
           <PiBookmarkSimpleBold />
-          Bookings
+          Orders
         </Link>
         <Link to="/admin/items" className='w-full h-[40px] text-[25px] font-bold flex justify-center items-center'>
           <LuSpeaker />
           Items
         </Link>
-        <button className='w-full h-[40px] text-[25px] font-bold flex justify-center items-center'>
+        <Link to="/admin/users" className='w-full h-[40px] text-[25px] font-bold flex justify-center items-center'>
           <FaRegUser />
           Users
-        </button>
+        </Link>
 
       </div>
       <div className='w-[calc(100vw-200px)] h-screen'>
         {/* මෙයද ස්තීර නැති ප්‍රදේශයක් නිසා මෙයත් Routes වලින් wrap කරගන්න ඕනෙ */}
-        <Routes path="/*">
-          <Route path="/bookings" element={<h2>Bookings</h2>} />
+        {userValidated && <Routes path="/*">
+          <Route path="/orders" element={<AdminOrdersPage/>} />
+          <Route path="/users" element={<AdminUsersPage/>}/>
           <Route path="/items" element={<AdminItemsPage/>}/>
           <Route path="/items/add" element={<AddItemPage/>}/>
           <Route path="/items/edit" element={<UpdateItemPage/>}/>
-        </Routes>
+        </Routes>}
 
       </div>
 
